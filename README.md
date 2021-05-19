bdbe13b8c18b886c52b769bf19af741258043603
GoEun Jeong <gogo8272@gmail.com>
2021-05-19 23:38:56 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Then - Carthage
+
+[Then](https://github.com/devxoul/Then)'s Carthage Support
---
+++ Then/Then.h
@@ -0,0 +1,18 @@
+//
+//  Then.h
+//  Then
+//
+//  Created by GoEun Jeong on 2021/05/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Then.
+FOUNDATION_EXPORT double ThenVersionNumber;
+
+//! Project version string for Then.
+FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Then/PublicHeader.h>
+
+
